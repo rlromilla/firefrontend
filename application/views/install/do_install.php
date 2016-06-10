6d6e0829e7ab21b74b2871e9b210a2c694064ad3
Rossette Romilla <rossette.romilla@digify.com.ph>
2016-06-10 16:29:23 +0800

    initial commit

---
+++ application/views/install/do_install.php
@@ -0,0 +1,15 @@
+<p><?php echo lang('in_complete_intro'); ?></p>
+
+<div class="well">
+    <p>
+        <?php echo lang('in_email') ?>: <b>admin@mybonfire.com</b><br/>
+        <?php echo lang('in_password') ?>: <b>password</b>
+    </p>
+</div>
+
+<h3><?php echo lang('in_complete_next') ?></h3>
+
+<ul>
+    <li><?php echo lang('in_complete_visit') ?> <a href="<?php echo site_url('admin') ?>"><?php echo lang('in_admin_area') ?></a></li>
+    <li><?php echo lang('in_complete_visit') ?> <a href="<?php echo site_url() ?>"><?php echo lang('in_site_front') ?></a></li>
+</ul>
